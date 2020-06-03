6894c4c47350489e77158ed66ecc041d15842954
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-06-03 13:39:26 -0300

    State and Immutability

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 
 import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0); // [valor do estado, função para atualizar o valor do estado]
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
     <div>
       <Header title='Helo World'/>
       <Header title='Título 2'/>
       <Header title='Título 3'/>
 
-      <h1>Conteúdo da aplicação</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
