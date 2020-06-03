00b2e8db0ca2ef0a63139d40afc38471cd3b3512
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-06-03 13:15:57 -0300

    JSX & Component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
 
+
+import Header from './Header';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    // JSX: Sintaxe de XML dentro do JavaScript
+    <div>
+      <Header />
+
+      <h1>Conteúdo da aplicação</h1>
+    </div>
   );
 }
 
